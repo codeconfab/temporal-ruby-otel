2a09ec75077ee3f75cc00c52b97a80ea61341f9e
Lar Van Der Jagt <lar@codeconfab.com>
2026-07-18 14:38:31 -0400

    Revert "Fix temporal shutdown warning in test"
    The latest version of the temporalio gem appears to fix this.
    
    This reverts commit 25282004dda042316b1e3d00dcfbdd905ac5daaf.

--- test/app_test.rb
+++ test/app_test.rb
@@ -9,18 +9,15 @@ class TemporalRubyOtelTest < Minitest::Test
     Temporalio::Testing::WorkflowEnvironment.start_local do |env|
       worker = TemporalRubyOtel.worker(env.client)
 
-      result = worker.run do
+      worker.run do
         result = TemporalRubyOtel.execute_workflow(
           client: env.client,
           workflow: :say_hello,
           args: 'some-name',
         )
 
+        assert_equal 'Hello, some-name!', result
       end
-
-      sleep 0.1
-
-      assert_equal 'Hello, some-name!', result
     end
   end
 end
